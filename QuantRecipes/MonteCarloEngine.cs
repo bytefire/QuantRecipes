dd5143c5f38b25de1eb5114526f2a49bcb6f287c
Okash Khawaja <okash.khawaja@gmail.com>
2013-03-24 08:26:55 +0000

    added Monte Carlo engine

---
+++ QuantRecipes/MonteCarloEngine.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantRecipes
+{
+    // uses Monte Carlo simulations to price options.
+    public class MonteCarloEngine
+    {
+        // Prices European option using Monte Carlo simulation
+        public double PriceEuropeanOption(double startingAssetValue, double interestRate, OptionType optionType, double strike,
+            double volatility, double timeStep, int numberOfTimeSteps)
+        {
+            int numberOfSimulations = 50;
+            object syncLock = new object();
+            double sumOfFutureValuesOfOption = 0.0;
+            Parallel.For(0, numberOfSimulations, simulationNumber =>
+                {
+                    double currentAssetValue = startingAssetValue, futureValueOfOption = 0.0;
+                    // each simulation
+                    for (int i = 0; i < numberOfTimeSteps; i++)
+                    {
+                        currentAssetValue = SimulateNextAssetValue(currentAssetValue, interestRate, timeStep, volatility);
+                    }
+                    futureValueOfOption = Math.Max((int)optionType * (currentAssetValue - strike), 0);
+                    // lock to ensure that each addition is atomic.
+                    // TODO: this is a bottleneck and overhead of locking not only
+                    //      makes this loop linear but slows it down even more due to locking overhead.
+                    lock (syncLock)
+                    {
+                        sumOfFutureValuesOfOption += futureValueOfOption;
+                    }
+                });
+            double averageFutureValueOfOption = sumOfFutureValuesOfOption / numberOfSimulations;
+            // compute present value of the average future value. here timeStep*numberOfTimeSteps gives total time to expiry
+            double optionValue = Math.Exp(-interestRate * timeStep * numberOfTimeSteps) * averageFutureValueOfOption;
+            return optionValue;
+        }
+
+        // Prices European option using Monte Carlo simulation. This uses linear for loop instead of Parallel
+        // NOTE: Linear is faster, probably because of the locking overhead in parallel version.
+        public double PriceEuropeanOptionLinear(double startingAssetValue, double interestRate, OptionType optionType, double strike,
+            double volatility, double timeStep, int numberOfTimeSteps)
+        {
+            int numberOfSimulations = 50;
+            object syncLock = new object();
+            double sumOfFutureValuesOfOption = 0.0;
+            for(int simulationNumber = 0;simulationNumber<numberOfSimulations; simulationNumber++)
+            {
+                double currentAssetValue = startingAssetValue, futureValueOfOption = 0.0;
+                // each simulation
+                for (int i = 0; i < numberOfTimeSteps; i++)
+                {
+                    currentAssetValue = SimulateNextAssetValue(currentAssetValue, interestRate, timeStep, volatility);
+                }
+                futureValueOfOption = Math.Max((int)optionType * (currentAssetValue - strike), 0);
+
+                sumOfFutureValuesOfOption += futureValueOfOption;
+            }
+            double averageFutureValueOfOption = sumOfFutureValuesOfOption / numberOfSimulations;
+            // compute present value of the average future value. here timeStep*numberOfTimeSteps gives total time to expiry
+            double optionValue = Math.Exp(-interestRate * timeStep * numberOfTimeSteps) * averageFutureValueOfOption;
+            return optionValue;
+        }
+
+        // uses the risk-neutral random walk to generate next asset value for the given parameters.
+        // this overload uses Box-Muller method to generate standard Normal random number.
+        private double SimulateNextAssetValue(double currentAssetValue, double interestRate, double timeStep, double volatility)
+        {
+            return SimulateNextAssetValue(currentAssetValue, interestRate, timeStep, volatility, (new Utilities()).GetBoxMullerRandomNumber);
+        }
+        // uses the risk-neutral random walk to generate next asset value for the given parameters.
+        private double SimulateNextAssetValue(double currentAssetValue, double interestRate, double timeStep,
+            double volatility, Func<double> getStandardNormalRandomNumber)
+        {
+            double phi = getStandardNormalRandomNumber();
+            double assetValueStep = interestRate * currentAssetValue * timeStep +
+                volatility * currentAssetValue * Math.Sqrt(timeStep) * phi;
+            double nextAssetValue = currentAssetValue + assetValueStep;
+            return nextAssetValue;
+        }
+    }
+}
---
+++ QuantRecipes/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantRecipes
+{
+    public class Utilities
+    {
+        // NOTE: Random must be instantiated on class-level so that it is initialised just once.
+        //       Otherwise it will be initialised everytime GetBoxMullerRandomNumber method is called,
+        //       the same sequence of random numbers will be generated. It's the same reason that this 
+        //       instance is static too.
+        private static Random rng = new Random();
+        // Generates standard normal random number by applying Box-Muller transform on a uniform random number.
+        public double GetBoxMullerRandomNumber()
+        {
+            // NOTE: Aparently there's a better way to generate uniform random numbers: Normal Deviates by Ratio-of-Uniforms
+            // (section 7.3.9 in Numerical Recipes)
+            double x, y, randomSumOfSquares;
+            do
+            {
+                x = 2 * rng.NextDouble() - 1;
+                y = 2 * rng.NextDouble() - 1;
+                randomSumOfSquares = x * x + y * y;
+            } while (randomSumOfSquares >= 1);
+            double boxMuller = x * Math.Sqrt(-2 * Math.Log(randomSumOfSquares) / randomSumOfSquares);
+            return boxMuller;
+        }
+    }
+}
